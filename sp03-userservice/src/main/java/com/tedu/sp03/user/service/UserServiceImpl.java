adcd4294eb07bbf6ef8f598a6ab82a9840c02023
Administrator <Administrator@tarena>
2019-11-04 11:45:59 +0800

    rabbitmq

--- sp03-userservice/src/main/java/com/tedu/sp03/user/service/UserServiceImpl.java
+++ sp03-userservice/src/main/java/com/tedu/sp03/user/service/UserServiceImpl.java
@@ -1,16 +1,17 @@
 package com.tedu.sp03.user.service;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
-
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.tedu.sp01.pojo.User;
 import com.tedu.sp01.service.UserService;
 import com.tedu.web.util.JsonUtil;
 
 import lombok.extern.slf4j.Slf4j;
+
+@RefreshScope
 @Slf4j
 @Service
 public class UserServiceImpl implements UserService{
